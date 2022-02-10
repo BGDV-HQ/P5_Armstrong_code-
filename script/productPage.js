8cc91b954ec40e53bbeca27d13af72403f6544b2
Donavan <donavan.mc@gmail.com>
2022-02-10 15:50:19 -0800

    Update productPage.js

--- script/productPage.js
+++ script/productPage.js
@@ -5,23 +5,10 @@ const displaySingleProduct = fetch('http://localhost:3000/api/products');
     return response.json();
 }).then(products => {          
     const singleProduct = singleProducts;
-    let singleDisplay = '';
 
-    for (let i in products) {   //adding each camera from the response into DOM
-    
-      display +=
-      document.getElementById('item__img').src = `${products[i].imageUrl}`;
+    document.getElementById('item__img').src = `${products.imageUrl}`;
         
          
-  document.getElementById('items').innerHTML = singleDisplay;
-  }
-}).catch(error => {                     // if "REJECT" clears the content of main container and displays information for user
-  document.getElementById('items').innerHTML = `
-      <div>
-          <h2>Sorry, an error occured...(${error})</h2>
-          <p>Please refresh your page or come back later while we fix the problem.</p>
-      </div>`;
-  console.log('Error: Fetch request failed!');
 
 });      
     
