51db2db740e8b01224d9c71b3c16ca775eca7478
Donavan <donavan.mc@gmail.com>
2022-02-11 16:49:50 -0800

    Update productPage.js

--- script/productPage.js
+++ script/productPage.js
@@ -3,10 +3,10 @@ const displaySingleProduct = fetch('http://localhost:3000/api/products');
 
   displaySingleProduct.then(response => {     
     return response.json();
-}).then(products => {          
+}).then(singleProducts => {          
     const singleProduct = singleProducts;
 
-    document.getElementById('item__img').src = `${products.imageUrl}`;
+    document.getElementById('item__img').src = `${singleProduct.imageUrl}`;
         
          
 
