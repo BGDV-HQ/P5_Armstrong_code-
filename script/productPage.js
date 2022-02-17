9317c150ed6829eea70f193c2c9ef5ceef800f05
HANI <68349763+blithe-dv@users.noreply.github.com>
2022-02-17 20:58:31 +0000

    Update productPage.js

--- script/productPage.js
+++ script/productPage.js
@@ -2,8 +2,23 @@ var str = window.location.href;
 var url = new URL(str);
 var idProduct = url.searchParams.get("id");
 
+// We spoke about querySelector last week (see below; lines; 6-10)
+console.log (idProduct);
+let article = "";
+
+const colorPicked = document. querySelector ("#colors");
+const quantityPicked = document.querySelector ("#quantity");
+
+getArticle ();
+
+// Retrieve the articles from the API
+
 function getArticle() {
-  const displaySingleProduct = fetch('http://localhost:3000/api/products' + idProduct);
+  // const displaySingleProduct = fetch('http://localhost:3000/api/products' + idProduct);
+  fetch ("http://localhost:3000/api/products/" + idProduct) //What is your "idProduct" conventionally named as - its red?
+  .then ((res) => {
+      return res.json ();
+  })
 
   displaySingleProduct.then(response => {     
     return response.json();
@@ -21,6 +36,68 @@ function getArticle() {
     console.log('Error: Fetch request failed!');
   
   });      
+}
+
+//STEPS REQUIRED
+// Breakdown of the API data in the DOM
+
+function getPost(article){
+    // Image insertion
+
+    // Modification of the title "h1
+
+    // Modification of the price
+
+    // Modification of the description
+
+    // Insertion of color options
+
+}
+
+//Shopping cart management
+function addToCart(article) {
+    const btn_sendBasket = document.querySelector("#addToCart");
+
+    //Listen to the basket with 2 non-zero color conditions and quantity between 1 and 100
+
+    //Recovery of the color choice
+                
+    //Recovery of the choice of quantity
+
+    //Retrieving the options of the item to add to the cart
+
+    };
+
+    //Initialisation of the local storage
+    let productLocalStorage =
+
+    //window pop-up
+    const popupConfirmation =() =>{
+        if(
+        }
+    }
+    
+    
+    //Import into local storage
+    //If the basket already contains at least 1 item
+    if (productLocalStorage) {
+    const resultFind = 
+          
+        //If the ordered product is already in the basket
+        if (resultFind) {
+            let newQuantity =
+                
+        //If the ordered product is not in the basket
+        } else {
+            productLocalStorage.push(
+        }
+    //If the basket is empty
+    } else {
+        productLocalStorage =[];
+        productLocalStorage.push(optionsProduct);
+        localStorage.setItem(
+    }}
+    });
 }
 
 
